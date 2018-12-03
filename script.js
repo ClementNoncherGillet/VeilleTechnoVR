885b5afaf13fafc3e34d81011e12ed6f3138fa47
ClementNoncherGillet <clem.noncher.g@gmail.com>
2018-12-03 22:22:03 +0100

    Rendu V3

---
+++ script.js
@@ -0,0 +1,10 @@
+if ( window.addEventListener ) {
+var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+window.addEventListener("keydown", function(e){
+kkeys.push( e.keyCode );
+if ( kkeys.toString().indexOf( konami ) >= 0 ) {
+alert('Dofus est un bon jeu contrairement à FF');
+window.location = "https://download.dofus.com/full/win/";
+}
+}, true);
+}
