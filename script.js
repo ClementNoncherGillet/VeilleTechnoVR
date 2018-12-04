d5a4f2fcb800b9fd955bd8474fb720fd7212e4d1
ClementNoncherGillet <clem.noncher.g@gmail.com>
2018-12-04 18:20:47 +0100

    Rendu final V3

--- script.js
+++ script.js
@@ -5,23 +5,7 @@ kkeys.push( e.keyCode );
 if ( kkeys.toString().indexOf( konami ) >= 0 ) {
 alert('Dofus est un bon jeu contrairement à FF');
 window.location = "https://download.dofus.com/full/win/";
-if (key == requiredKey) {
 
-    // move to the next key in the konami code sequence
-    konamiCodePosition++;
-
-    // if the last key is reached, activate cheats
-    if (konamiCodePosition == konamiCode.length) {
-      activateCheats();
-      konamiCodePosition = 0;
-    }
-  } else {
-    konamiCodePosition = 0;
-  }
-});
-
-function activateCheats() {
-  document.body.style.backgroundImage = "url('https://gamespell.fr/wp-content/uploads/2017/10/dofus.jpg')";
 }
 }, true);
 }
